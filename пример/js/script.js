81c20296610ae39d08911c9bdd35e864a17cf39c
CutSmokey <37474381+CutSmokey@users.noreply.github.com>
2018-05-17 11:58:46 +0300

    rew

--- пример/js/script.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function(){
-	$("#btn-down").on("click","a", function (event) {
-		event.preventDefault();
-		var id  = $(this).attr('href'),
-			top = $(id).offset().top;
-		$('body,html').animate({scrollTop: top}, 650);
-	});
-});
